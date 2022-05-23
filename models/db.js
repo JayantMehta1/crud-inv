f452041b4e74f63a57b82cbd54690c6aeb44ef9c
Jayant Mehta <jay9mehta@gmail.com>
2022-05-23 11:31:46 -0400

    update

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var URI = "mongodb+srv://crud:abcd@cluster0.jkz5k.mongodb.net/?retryWrites=true&w=majority";
+var URI = "mongodb+srv://crud:abcd@cluster0.i40fv.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(URI)
 .then(()=>console.log('DB CONNECTED'))
